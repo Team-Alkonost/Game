269cf107a4ae29ec43871a24b2f673e71f31de31
petkov75 <tsvetanpetkov@abv.bg>
2015-12-12 23:52:53 +0200

    ver 1.1.3

--- Alkonost2/Alkonost2/TitleScreen.cs
+++ Alkonost2/Alkonost2/TitleScreen.cs
@@ -10,10 +10,11 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Alkonost2
 {
-   public  class TitleScreen : GameScreen
+    public class TitleScreen : GameScreen
     {
         KeyboardState keyState;
         SpriteFont font;
+        SpriteFont font2;
         Texture2D womanHero;
         Texture2D knights;
 
@@ -22,6 +23,7 @@ namespace Alkonost2
 
             base.LoadContent(Content);
             if (font == null) font = Content.Load<SpriteFont>("Font1");
+            if (font2 == null) font2 = Content.Load<SpriteFont>("Font2");
             if (womanHero == null) womanHero = Content.Load<Texture2D>("Sprites/womanH");//knights
             if (knights == null) knights = Content.Load<Texture2D>("Sprites/knights");
         }
@@ -39,9 +41,18 @@ namespace Alkonost2
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(font, "Choose Battle Heroes : Press \"Z\" main menue", new Vector2(100, 10), Color.Black);
+            spriteBatch.DrawString(font, "Choose Battle Heroes : Press \"Z\"  back  to main menue", new Vector2(100, 10), Color.Black);
             spriteBatch.Draw(womanHero, new Vector2(100, 30), Color.White);
+            spriteBatch.DrawString(font, "Life : 100 ", new Vector2(300, 100), Color.Blue);
+            spriteBatch.DrawString(font, "Damage : 50 ", new Vector2(300, 140), Color.Blue);
+            spriteBatch.DrawString(font, "Armor : 10 ", new Vector2(300, 180), Color.Blue);
+            spriteBatch.DrawString(font, "Mouvement : 100 ", new Vector2(300, 220), Color.Blue);
             spriteBatch.Draw(knights, new Vector2(80, 330), Color.White);
+            spriteBatch.DrawString(font, "Life : 100 ", new Vector2(300, 380), Color.Red);
+            spriteBatch.DrawString(font, "Damage : 70 ", new Vector2(300, 420), Color.Red);
+            spriteBatch.DrawString(font, "Armor : 15 ", new Vector2(300, 460), Color.Red);
+            spriteBatch.DrawString(font, "Mouvement : 80 ", new Vector2(300, 500), Color.Red);
         }
+
     }
 }
