779732aab55e8255d50dfcb41eacbe2c958a0cdb
MiroslavTonev <mirotonev@gmail.com>
2015-12-13 18:48:05 +0200

    Added Player and some items

---
+++ Alkonost2/Alkonost2/Models/Items/Sheald.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alkonost2.Models.Items
+{
+    public class Sheald : Item
+    {
+        private const double DefautMovementBonus = 0;
+        private const double DefautDamage = 0;
+        private const double DefautArmor = 40;
+        private const double DefautHealth = 50;
+        private const int DefautCritChannce = 0;
+
+        public Sheald() : base(DefautDamage, DefautArmor, DefautHealth, DefautMovementBonus, DefautCritChannce)
+        {
+        }
+    }
+}
---
+++ Alkonost2/Alkonost2/Models/ModelsInterfases/IItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Alkonost2.Models.ModelsInterfases
+{
+    public interface IItem
+    {
+        double BonusDamage { get; }
+        double BonusArmor { get; }
+        double BonusHealth { get; }
+        double BonusMovement { get; }
+        int BonusCritChance { get; }
+    }
+}
